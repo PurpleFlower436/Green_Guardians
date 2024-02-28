2d640ad742519db89f0d267ee9b81a3b7bcfd091
PurpleFlower436 <bhargvih12@gmail.com>
2024-02-27 22:30:51 -0600

    Fixed positioning for recycle bin sprite

--- rexs_world/lib/screens/recycle_sorter.dart
+++ rexs_world/lib/screens/recycle_sorter.dart
@@ -15,11 +15,12 @@ class recycleSorter extends FlameGame with PanDetector {
   late Player player;
   @override
   Future<void> onLoad() async {
-    await super.onLoad();
+    await super.onLoad(); // loads the game
 
     player = Player();
-    add(player);
-    add(NatureBackground());
+
+    add(NatureBackground()); //This adds the nature background to the screen
+    add(player); //This adds the recycle bin sprite to the screen
   }
 
   @override
@@ -29,17 +30,13 @@ class recycleSorter extends FlameGame with PanDetector {
 }
 
 class Player extends SpriteComponent with HasGameRef<recycleSorter> {
-  Player()
-      : super(
-          size: Vector2(200, 200),
-          anchor: Anchor.center,
-        );
+  Player() : super(size: Vector2(200, 200), anchor: Anchor.bottomCenter);
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     sprite = await gameRef.loadSprite('recycle_bin.png');
-    position = gameRef.size / 6;
+    position = Vector2(gameRef.size.x / 2, gameRef.size.y);
   }
 
   void move(Vector2 delta) {
