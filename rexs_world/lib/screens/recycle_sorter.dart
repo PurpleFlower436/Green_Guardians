063c0a15211fdeab6b034e680b641602875e0480
PurpleFlower436 <bhargvih12@gmail.com>
2024-02-28 21:13:00 -0600

    started to add the code to handle keyboard events

--- rexs_world/lib/screens/recycle_sorter.dart
+++ rexs_world/lib/screens/recycle_sorter.dart
@@ -3,24 +3,45 @@
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 //import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flame/input.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(GameWidget(game: recycleSorter()));
 }
 
-class recycleSorter extends FlameGame with PanDetector {
+class recycleSorter extends FlameGame with PanDetector, KeyboardEvents {
   late Player player;
   @override
   Future<void> onLoad() async {
     await super.onLoad(); // loads the game
 
+    if (kIsWeb) {
+    } else if (Platform.isAndroid || Platform.isIOS) {}
+
     player = Player();
 
     add(NatureBackground()); //This adds the nature background to the screen
     add(player); //This adds the recycle bin sprite to the screen
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    final isKeyDown = event is RawKeyDownEvent;
+
+    if (isKeyDown && keysPressed.contains(LogicalKeyboardKey.altLeft)) {
+      player.moveLeft();
+    } else if (isKeyDown && keysPressed.contains(LogicalKeyboardKey.altRight)) {
+      player.moveRight();
+    }
+    return KeyEventResult.handled;
   }
 
   @override
@@ -37,6 +58,14 @@ class Player extends SpriteComponent with HasGameRef<recycleSorter> {
     await super.onLoad();
     sprite = await gameRef.loadSprite('recycle_bin.png');
     position = Vector2(gameRef.size.x / 2, gameRef.size.y);
+  }
+
+  void moveLeft() {
+    position.x -= 10;
+  }
+
+  void moveRight() {
+    position.x += 10;
   }
 
   void move(Vector2 delta) {
