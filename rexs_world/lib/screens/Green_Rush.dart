57fb017b3f15d89a323e8773c7a0a126a452ae46
PurpleFlower436 <bhargvih12@gmail.com>
2024-03-12 02:56:07 -0500

    Started to build Green_Rush

---
+++ rexs_world/lib/screens/Green_Rush.dart
@@ -0,0 +1,21 @@
+import 'package:flame_audio/flame_audio.dart';
+import 'package:flame/collisions.dart';
+import 'package:flame/experimental.dart';
+import 'package:flame/game.dart';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/palette.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/services.dart';
+import 'package:rexs_world/screens/Green_Rush_instructions.dart';
+
+void main() {
+  runApp(GameWidget(game: Green_Rush()));
+}
+
+class Green_Rush extends FlameGame
+    with PanDetector, KeyboardEvents, HasCollisionDetection {}
