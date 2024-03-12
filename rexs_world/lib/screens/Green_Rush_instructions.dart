24f0d5a4cbe3e6d090d9e42404f64470e68ab341
PurpleFlower436 <bhargvih12@gmail.com>
2024-03-12 02:46:56 -0500

    created files for Green Rush load screen and Green Rush game

--- rexs_world/lib/screens/Green_Rush_instructions.dart
+++ rexs_world/lib/screens/Green_Rush_instructions.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:rexs_world/screens/recycle_sorter.dart';
 
 class GreenRushInstructions extends StatelessWidget {
@@ -7,9 +10,55 @@ class GreenRushInstructions extends StatelessWidget {
 
   const GreenRushInstructions({super.key, required this.game});
 
+ 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Text(
+            """ Hello! My name is Dash and my world is in danger. My home is in a beautiful land where ne and my animal friends live in peace and harmony. Our town runs on sustainability which allows us to ensure our town is safe from dangers. But I heard that some evil construction workers want to bulldoze our town and build a mansion on it. 
+              Help me stop them by completing these 2 challenges so we can save my home!""",
+            style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
+          ).animate().scale(duration: 500.ms)),
+          // ignore: prefer_const_constructors
+          Image(
+              image: AssetImage('assets/images/Dashatar_Bonus_NakedDash.png')),
+          SizedBox(height: 90),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => GameScreen()));
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromARGB(255, 166, 231, 151),
+            ),
+            child: Text(
+              "Play",
+              style: TextStyle(
+                  fontSize: 50, color: Color.fromARGB(255, 238, 176, 83)),
+            ),
+          )
+        ],
+      ),
+    );
   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 }
